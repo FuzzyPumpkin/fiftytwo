2910781ca899f8d2a31b5519dc0a545ad89cd8d3
Kari <kariminger@gmail.com>
2020-01-08 19:25:37 -0600

    Post page layout
    Route created, header complete, copy placeholder text

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname + "/public"));
 
+app.get("/post", function(req, res){
+    res.render("post");
+ });
+
 app.get("/", function(req, res){
     res.render("home");
  });
