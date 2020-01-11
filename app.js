227d2ba3ae648ee693e151730ebffb324b5ae0ff
Kari <kariminger@gmail.com>
2020-01-10 19:23:19 -0600

    Successful database integration

--- app.js
+++ app.js
@@ -2,6 +2,35 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+
+//DB connection
+mongoose.connect('mongodb+srv://kariminger:12mI94Zzr94P2AY@cluster0-fomve.mongodb.net/test?retryWrites=true&w=majority', 
+    {
+        useNewURLParser: true,
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(()=> {
+        console.log("connected to database");
+    }).catch(err => {
+        console.log("ERROR:", err.message);
+    });
+
+const PostSchema = new mongoose.Schema({
+    weekNumber: String,
+    projectName: String,
+    GithubURL: String,
+    siteURL: String,
+    processTxt: String,
+    challengesTxt: String,
+    takeawaysTxt: String,
+    imageName: String,
+    tech: Array
+});
+const Post = mongoose.model("Post", PostSchema);
+
+
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -18,8 +47,24 @@ app.get("/post", function(req, res){
     res.render("post");
  });
 
-app.get("/", function(req, res){
-    res.render("home");
+app.get("/", async (req, res) => {
+    let post = await Post.create({
+        weekNumber: "0",
+        projectName: "Test project name",
+        GithubURL: "https://github.com/FuzzyPumpkin/fiftytwo",
+        siteURL: "https://fuzzypumpkin.github.io/",
+        processTxt: "I followed a process, really",
+        challengesTxt: "Challenges are just tests with a longer name.",
+        takeawaysTxt: "Calgon!",
+        imageName: "testImg",
+        tech: ["Node.js", "Sass"]}
+    );
+    res.send(post)
+    // res.render("home");
  });
+
+
+
+
 
 app.listen(port, function() {console.log("server started");});
